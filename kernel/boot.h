eefa2e89393d8f57a02df8870b2b4166c1aac969
Dang Libin <libin.dang@gmail.com>
2026-08-14 11:27:57 +0800

    feat(mmix): establish the MMIX boot path

---
+++ kernel/boot.h
@@ -0,0 +1,20 @@
+#ifndef XV6_MMIX_BOOT_H
+#define XV6_MMIX_BOOT_H
+
+#include "types.h"
+#include "bootinfo.h"
+
+// State retained by the minimal MMIX boot path for early diagnostics.
+struct mmix_boot_state {
+  uint64 startup_cpu_id;
+  uint64 bootinfo_pa;
+  int bootinfo_status;
+  struct mmix_bootinfo info;
+};
+
+extern struct mmix_boot_state mmix_boot;
+
+void mmix_start(uint64 startup_cpu_id, uint64 bootinfo_pa)
+    __attribute__((noreturn));
+
+#endif
--- kernel/start.c
+++ kernel/start.c
@@ -1,66 +1,24 @@
-#include "types.h"
-#include "param.h"
-#include "memlayout.h"
-#include "riscv.h"
-#include "defs.h"
+#include "boot.h"
 
-void main();
-void timerinit();
+struct mmix_boot_state mmix_boot;
 
-// entry.S needs one stack per CPU.
-__attribute__((aligned(16))) char stack0[4096 * NCPU];
+static void
+mmix_wait(void) __attribute__((noreturn));
 
-// entry.S jumps here in machine mode on stack0.
-void
-start()
+static void
+mmix_wait(void)
 {
-  // set M Previous Privilege mode to Supervisor, for mret.
-  unsigned long x = r_mstatus();
-  x &= ~MSTATUS_MPP_MASK;
-  x |= MSTATUS_MPP_S;
-  w_mstatus(x);
-
-  // set M Exception Program Counter to main, for mret.
-  // requires gcc -mcmodel=medany
-  w_mepc((uint64)main);
-
-  // disable paging for now.
-  w_satp(0);
-
-  // delegate all interrupts and exceptions to supervisor mode.
-  w_medeleg(0xffff);
-  w_mideleg(0xffff);
-  w_sie(r_sie() | SIE_SEIE | SIE_STIE);
-
-  // configure Physical Memory Protection to give supervisor mode
-  // access to all of physical memory.
-  w_pmpaddr0(0x3fffffffffffffull);
-  w_pmpcfg0(0xf);
-
-  // enable hardware updates of page table A and D bits
-  w_menvcfg(r_menvcfg() | MENVCFG_ADUE);
-
-  // ask for clock interrupts.
-  timerinit();
-
-  // keep each CPU's hartid in its tp register, for cpuid().
-  int id = r_mhartid();
-  w_tp(id);
-
-  // switch to supervisor mode and jump to main().
-  asm volatile("mret");
+  for (;;)
+    asm volatile("SWYM 0, 0, 0");
 }
 
-// ask each hart to generate timer interrupts.
 void
-timerinit()
+mmix_start(uint64 startup_cpu_id, uint64 bootinfo_pa)
 {
-  // enable the sstc extension (i.e. stimecmp).
-  w_menvcfg(r_menvcfg() | MENVCFG_STCE);
+  mmix_boot.startup_cpu_id = startup_cpu_id;
+  mmix_boot.bootinfo_pa = bootinfo_pa;
+  mmix_boot.bootinfo_status =
+      mmix_bootinfo_decode(startup_cpu_id, bootinfo_pa, &mmix_boot.info);
 
-  // allow supervisor to use stimecmp and time.
-  w_mcounteren(r_mcounteren() | 2);
-
-  // ask for the very first timer interrupt.
-  w_stimecmp(r_time() + 1000000);
+  mmix_wait();
 }
